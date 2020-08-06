6f266404935236419ace7bd4878838337b2293e0
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-08-06 00:52:10 -0400

    created more files, prepared api.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-ruter-dom";
+import Home from './pages/Home';
+import Jumbotron from './components/Jumbotron'
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Jumbotron />
+        <Wrapper>
+          <Home />
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
-
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import API from "../utils.API.js";
+// import Jumbotron from "../components/Jumbotron";
+// import Container from "../components/Container";
+// import Row from "../components/Row";
+// import Col from "../components/Col";
+// import Search from "../components/Search";
+import { Link } from "react-router-dom";
+
+
+
