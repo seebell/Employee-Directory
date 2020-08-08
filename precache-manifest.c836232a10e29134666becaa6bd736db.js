827893abf20668360228897c46774166c451820a
Sibel Cidem <sibsel_cidem@hotmail.com>
2020-08-08 08:06:57 -0400

    Updates

---
+++ precache-manifest.c836232a10e29134666becaa6bd736db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65b516e597a716ead6a0925029dc81e",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "e707806266c541534485",
+    "url": "/Employee-Directory/static/css/main.4061849c.chunk.css"
+  },
+  {
+    "revision": "7eba9d99d66461c69b25",
+    "url": "/Employee-Directory/static/js/2.af614a9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Employee-Directory/static/js/2.af614a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e707806266c541534485",
+    "url": "/Employee-Directory/static/js/main.6d42d475.chunk.js"
+  },
+  {
+    "revision": "1b13436491d4ecd8c222",
+    "url": "/Employee-Directory/static/js/runtime-main.39ec69b2.js"
+  }
+]);
